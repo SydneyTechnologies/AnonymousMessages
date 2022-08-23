45fdedbec8ac918ffe6b2d638ecb8516e5326047
sydney <sydney.idundun@gmail.com>
2022-08-23 21:19:51 +0200

    created a message model that has a one to many relationship with a user

--- message/models.py
+++ message/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class Message(models.model):
+    target = models.ForeignKey(get_user_model())
+    message = models.TextField()
+    @property
+    def get_title(self):
+        return self.message[0:10]
+    
+    def __str__(self) -> str:
+        return self.get_title
